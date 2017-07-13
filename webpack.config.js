942ce0b9e6f3f254887bf70bfd75ee73beb985bc
Ke Yang <yangke687@gmail.com>
2017-07-13 22:13:46 +0800

    Setting up CSS loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
         test: /\.js$/,
         exclude: path.resolve(__dirname, "node_modules")
       },
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/,
+      }
     ]
   }
 };
