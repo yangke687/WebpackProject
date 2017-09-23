a5902512aac5e9caebcb3d1dd52549f9cce959f4
Ke Yang <yangke687@gmail.com>
2017-09-23 15:56:57 +0800

    webpack-dev-middleware in 'production' mode

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express');
-const webpackMiddleware = require('webpack-dev-middleware');
-const webpackConfig = require('./webpack.config.js');
-const webpack = require('webpack');
-
+const path = require('path');
 const app = express();
-app.use(webpackMiddleware(webpack(webpackConfig)));
 
-app.listen(3050, () => {
-  console.log('Hey, I am listening');
+if( process.env.NODE_ENV !== 'production' ) {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpackConfig = require('./webpack.config.js');
+  const webpack = require('webpack');
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+} else {
+  app.use(express.static('dist'));
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+  });
+}
+
+app.listen(process.env.PORT || 3050, () => {
+  const mode = process.env.NODE_ENV;
+  console.log(`Hey, I am listening on mode: ${mode}`);
 });
 
