af6da9a62398136d121ce511fca2075b58d3cbb9
Ke Yang <yangke687@gmail.com>
2017-09-23 15:38:29 +0800

    Integrate node server with 'webpack-dev-middleware'

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('./webpack.config.js');
+const webpack = require('webpack');
 
 const app = express();
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 app.listen(3050, () => {
   console.log('Hey, I am listening');
